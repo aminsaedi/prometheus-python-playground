5403ad5637a2deff04d96fce9ab73a7fa7bec85c
Amin Saedi <amin.di.sam@gmail.com>
2023-12-17 17:14:08 +0000

    Added Histogram metric

--- main.py
+++ main.py
@@ -1,39 +1,16 @@
 import http.server
-from prometheus_client import start_http_server, Counter, Gauge, Summary
-import time
+from prometheus_client import start_http_server, Histogram
 
 
-TOTAL_REQUESTS = Counter(
-    "hello_worlds_total", "Hello Worlds requested of the HTTP server."
-)
-
-
-IN_PROGRESS = Gauge(
-    "hello_worlds_in_progress",
-    "Hello Worlds in progress.",
-)
-
-LAST = Gauge(
-    "hello_worlds_last_time_seconds",
-    "The last time a Hello World was served.",
-)
-
-LATENCY = Summary(
-    "hello_worlds_latency_seconds",
-    "Time for a request Hello World.",
-)
+LATENCY = Histogram("hello_world_latency_seconds", "Description of histogram")
 
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
-    @IN_PROGRESS.track_inprogress()
+    @LATENCY.time()
     def do_GET(self):
-        start = time.time()
-        TOTAL_REQUESTS.inc()
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello World - No CI 7")
-        LATENCY.observe(time.time() - start)
-        LAST.set_to_current_time()
 
 
 if __name__ == "__main__":
